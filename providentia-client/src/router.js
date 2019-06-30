c84d384d82a195a9fc9af85da5965d2695b77e35
Chiara Galiazzo <CHIARA@Air-di-Chiara.home-life.hub>
2019-07-01 00:09:28 +0200

    fixes

--- providentia-client/src/router.js
+++ providentia-client/src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Students from "./views/Students.vue";
+import Investors from "./views/Investors.vue";
 
 Vue.use(Router);
 
@@ -12,6 +14,16 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
+    },
+    {
+      path: "/students",
+      name: "students",
+      component: Students
+    },
+    {
+      path: "/investors",
+      name: "investors",
+      component: Investors
     },
   ]
 });
--- providentia-client/src/views/Investors.vue
+++ providentia-client/src/views/Investors.vue
@@ -1,5 +1,12 @@
 <template>
-
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h1>Investors</h1>
+        <h4>Make your investment</h4>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
