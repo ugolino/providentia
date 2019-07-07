4167dcb11d4e270d925f50a9906a162a79dc07d0
ugolino <dan.ugolini@gmail.com>
2019-07-07 20:44:08 +0200

    design fix

--- providentia-client/src/views/Investors.vue
+++ providentia-client/src/views/Investors.vue
@@ -40,9 +40,18 @@
             <v-flex xs12 md3 class="text-sm-center">
               <v-btn
                 color="primary elevation-0"
-                @click="selectedSchool === index ? selectAllStudents(school) : selectedSchool = index"
+                @click="selectedSchool === index ? selectedSchool = null : selectedSchool = index"
                 >
-                {{ selectedSchool === index ? 'Invest in all students' : 'Select Students' }}
+                {{ selectedSchool === index ? 'close' : 'Select Students' }}
+              </v-btn>
+              <br>
+              <v-btn
+                v-if="selectedSchool === index"
+                flat
+                color="primary elevation-0"
+                @click="school.students.every(elem => selectedStudents.indexOf(elem) > -1) ? deselectAllStudents(school) : selectAllStudents(school) "
+                >
+                {{ school.students.every(elem => selectedStudents.indexOf(elem) > -1) ? 'Deselect All' : 'Select All' }}
               </v-btn>
               
             </v-flex>
@@ -67,7 +76,7 @@
               </v-flex>
               <v-flex xs4 md2 class="text-sm-center">
                 <h4 class="subtitle">
-                  <a :href="student.github" v-if="student.github != ''">
+                  <a :href="student.github" target="_blank" v-if="student.github != ''">
                     <img src="https://img.icons8.com/material-sharp/128/454545/github.png" height="30px" />
                   </a>
                 </h4>
@@ -82,9 +91,59 @@
       </v-flex>
     </v-layout>
     <v-footer fixed class="pa-5" color="secondary">
-      <v-layout row wrap>
+      <v-layout align-center justify-space-between row wrap>
         <v-flex xs-12>
-           <h4>You selected <b>{{selectedStudents.length}}</b> students</h4>
+            
+          <v-btn
+            flat
+            color="red darken-3"
+            @click.stop="selectedStudentsDialog = true"
+          >
+            You selected {{selectedStudents.length}} students
+          </v-btn>
+
+          <v-dialog
+            v-model="selectedStudentsDialog"
+            max-width="800"
+          >
+            <v-card light>
+              <v-card-title class="title secondary white--text" >Review Students</v-card-title>
+              <v-card-text>
+                <v-layout v-if="selectedStudents.length === 0">
+                  No students selected
+                </v-layout>
+                <v-layout v-else v-for="student in selectedStudents" :key="student.id">
+                  <v-flex xs6 md3 pl-3>
+                    <h4 >{{student.name}}</h4>
+                  </v-flex>
+                  <v-flex xs4 md2 class="text-sm-center">
+                    <h4 class="subtitle">{{student.enrolled}}</h4>
+                    <p>enroll days</p>
+                  </v-flex>
+                  <v-flex xs4 md2 class="text-sm-center">
+                    <h4 class="subtitle">{{student.internship}}</h4>
+                    <p>internship</p>
+                  </v-flex>
+                  <v-flex xs4 md2 class="text-sm-center">
+                    <h4 class="subtitle">
+                      <a :href="student.github" target="_blank" v-if="student.github != ''">
+                        <img src="https://img.icons8.com/material-sharp/128/454545/github.png" height="30px" />
+                      </a>
+                    </h4>
+                  </v-flex>
+                    <v-flex xs4 md3 class="text-sm-center">
+                      <v-icon v-if="selectedStudents.includes(student)" @click="addStudent(student)">check</v-icon>
+                      <v-icon v-else @click="addStudent(student)">check_box_outline_blank</v-icon>
+                  </v-flex>
+                </v-layout>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+    
         </v-flex>
         <v-spacer></v-spacer>
         <v-flex xs-12>
@@ -112,7 +171,8 @@ import { mapState } from 'vuex'
     data() {
       return {
         selectedSchool: null,
-        selectedStudents: []
+        selectedStudents: [],
+        selectedStudentsDialog: false
       }
     },
 
@@ -138,8 +198,16 @@ import { mapState } from 'vuex'
         school.students.map((student, index) => {
           if (!this.selectedStudents.includes(student)) {
             this.selectedStudents.push(student)
-          } else {
-            this.selectedStudents.shift(student)
+          }
+        });
+      },
+      deselectAllStudents(school){
+        school.students.map((student, index) => {
+          if (this.selectedStudents.includes(student)) {
+           var index = this.selectedStudents.indexOf(student);
+            if (index > -1) {
+              this.selectedStudents.splice(index, 1);
+            }
           }
         });
       }
